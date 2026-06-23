52c39a5ececceca2e51516458d560a5ac1f1336c
Constant <constant5560@gmail.com>
2026-06-24 00:07:34 +0800

    finish 08 - concurrent

--- internal/engine/loop.go
+++ internal/engine/loop.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"sync"
 
 	"github.com/Constant2502/constant-tiny-claw/internal/provider"
 	"github.com/Constant2502/constant-tiny-claw/internal/schema"
@@ -83,26 +84,65 @@ func (e *AgentEngine) Run(ctx context.Context, userPrompt string) error {
 			break
 		}
 
-		log.Printf("[Engine] 模型请求调用 %d 个工具...\n", len(actionResp.ToolCalls))
+		log.Printf("[Engine] 模型并发请求调用 %d 个工具...\n", len(actionResp.ToolCalls))
 
-		for _, toolCall := range actionResp.ToolCalls {
-			log.Printf(" -> 🛠执行工具： %s, 参数: %s\n", toolCall.Name, string(toolCall.Arguments))
+		//核心改造，从串行改成并行。
+		//1.预分配一个固定长度的切片，用于安全地存放各个并发工具的执行结果，长度和tool calls的数量完全一致。
+		observationMsgs := make([]schema.Message, len(actionResp.ToolCalls))
 
-			result := e.registry.Execute(ctx, toolCall)
+		//2.声明WaitGroup用于阻塞等待所有协程完成。
+		var wg sync.WaitGroup
 
-			if result.IsError {
-				log.Printf(" -> ❌ 工具执行报错: %s\n", result.Output)
-			} else {
-				log.Printf(" -> ✅ 工具执行成功 (返回 %d 字节)\n", len(result.Output))
-			}
+		for i, toolCall := range actionResp.ToolCalls {
+			wg.Add(1)
 
-			observationMsg := schema.Message{
-				Role:       schema.RoleUser,
-				Content:    result.Output,
-				ToolCallID: toolCall.ID,
-			}
-			contextHistory = append(contextHistory, observationMsg)
+			go func(idx int, call schema.ToolCall) {
+				defer wg.Done()
+
+				log.Printf("    -> [Go-%d] 🛠 触发并发执行:  %s\n", idx, call.Name)
+
+				result := e.registry.Execute(ctx, call)
+
+				if result.IsError {
+					log.Printf("    -> [Go-%d] ❌ 工具执行报错:  %s\n", idx, call.Name)
+				} else {
+					log.Printf("    -> [Go-%d] ✅ 工具执行成功（返回 %d 字节)  \n", idx, len(result.Output))
+				}
+
+				obsMsg := schema.Message{
+					Role:       schema.RoleUser,
+					Content:    result.Output,
+					ToolCallID: call.ID,
+				}
+
+				observationMsgs[idx] = obsMsg
+			}(i, toolCall)
 		}
+
+		wg.Wait()
+		log.Println("[Engine] 所有并发工具执行完毕，开始聚合观察结果(Observation)...")
+
+		for _, obs := range observationMsgs {
+			contextHistory = append(contextHistory, obs)
+		}
+		//for _, toolCall := range actionResp.ToolCalls {
+		//	log.Printf(" -> 🛠执行工具： %s, 参数: %s\n", toolCall.Name, string(toolCall.Arguments))
+		//
+		//	result := e.registry.Execute(ctx, toolCall)
+		//
+		//	if result.IsError {
+		//		log.Printf(" -> ❌ 工具执行报错: %s\n", result.Output)
+		//	} else {
+		//		log.Printf(" -> ✅ 工具执行成功 (返回 %d 字节)\n", len(result.Output))
+		//	}
+		//
+		//	observationMsg := schema.Message{
+		//		Role:       schema.RoleUser,
+		//		Content:    result.Output,
+		//		ToolCallID: toolCall.ID,
+		//	}
+		//	contextHistory = append(contextHistory, observationMsg)
+		//}
 	}
 
 	return nil
